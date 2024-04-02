d1ce5c4db7843383852600cc2f4f848f48a9ab2b
Daniel <159606732+Danielbot01@users.noreply.github.com>
2024-04-01 23:42:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Monovan\nBot', {
+say('The Tazmania\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Monovan`, {
+say(`Bot creado por Լƛ ʆЄƑƛ`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
